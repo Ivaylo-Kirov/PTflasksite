6889700f82b1ac5542323f754661fe6348feaaf1
Ivaylo-Kirov <ivo.kirov@gmail.com>
2019-09-10 13:02:56 -0400

    Round 2

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 pipenv for a workflow similar to Node > installed Flask > and setup basic routes > one of which does render_template and passes a variable to it > url_for static didn't work for me so I couldn't do a style.css for now
+
+* Round 2 - noticed again the simplicity in Flask - added another /api route which returns a JSON response
--- index.py
+++ index.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
@@ -10,6 +10,15 @@ def index():
 @app.route('/about/<name>')
 def about(name=None):
     return render_template('about.html', name=name)
+
+@app.route('/api')
+def api():
+    responseData = {
+        'id': 1,
+        'name': 'ivo',
+        'roles': ['admin', 'user']
+    }
+    return jsonify(responseData)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5001, debug=True)
